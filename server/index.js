07010963de666829c07de05085aadd9caedfeb82
alok-112 <a.thakurm007@gmail.com>
2024-02-05 09:46:43 +0530

    added env

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import mongoose from 'mongoose'
 import cors from 'cors'
+import dotenv from 'dotenv'
+dotenv.config()
 
 const app = express()
 
@@ -9,15 +11,15 @@ app.use(bodyParser.json({ limit: "20mb", extended: true }))
 app.use(bodyParser.urlencoded({ limit: "20mb", extended: true }))
 app.use(cors())
 
-const CONNECTION_URL = process.env.MONGO_DB_URL || 'mongodb+srv://pictalesadmin:C9O9haolI0SQtJXo@pictales.e7ulnj9.mongodb.net/?retryWrites=true&w=majority'
-const PORT = process.env.PORT || 8000
+const CONNECTION_URL = process.env.MONGO_DB_URL;
+const PORT = process.env.PORT;
 
 const clientOptions = { serverApi: { version: '1', strict: true, deprecationErrors: true } };
 
 mongoose.connect(CONNECTION_URL, clientOptions)
-.then(() => app.listen(PORT, () => {
-    console.log("Connected to MongoDB Cluster")
-    console.log(`Server running on PORT: ${PORT}`)
-}))
-.catch(err => console.log(err.message))
+    .then(() => app.listen(PORT, () => {
+        console.log("Connected to MongoDB Cluster")
+        console.log(`Server running on PORT: ${PORT}`)
+    }))
+    .catch(err => console.log(err.message))
 
