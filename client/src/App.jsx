c8c89ddfbc2669a16573399471db2a14af9d1a2c
alok-112 <a.thakurm007@gmail.com>
2024-02-05 09:14:57 +0530

    added hello world

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,10 @@
 function App() {
-  return;
-  <div className="w-full h-screen bg-slate-900 text-red-600">Hello</div>;
+  return (
+    <>
+      <div className="w-full h-screen bg-slate-900 text-red-600">Hello</div>
+      <div className="w-full h-screen bg-slate-900 text-red-600">World</div>
+    </>
+  );
 }
 
 export default App;
